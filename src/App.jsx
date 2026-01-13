6686265aad01d56ae028cb25026e64b79735d5d1
Jason Sun <cryojs@gmail.com>
2026-01-13 09:19:29 -0500

    Initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import { BsChatSquareHeartFill, BsFillSunFill, BsFillMoonFill } from "react-icons/bs"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    return (
+        <>
+            {/* Navbar */}
+            <div className="sticky top-0 z-50 navbar bg-base-200 px-2 sm:px-4 py-2 shadow-md flex items-center min-h-0">
+                {/* Title */}
+                <div className="flex items-center gap-2 text-xl font-bold">
+                    <BsChatSquareHeartFill className="text-2xl" />
+                    <span className="text-xl font-mono uppercase">
+                        Hooked
+                    </span>
+                </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                {/* Actions */}
+                <div className="ml-auto flex items-center gap-1 sm:gap-2">
+                    {/* Theme Toggle */}
+                    <label
+                        className="toggle toggle-sm sm:toggle-md text-base-content ml-2"
+                    >
+                        <input type="checkbox" value="dark" className="theme-controller"/>
+                        <BsFillSunFill />
+                        <BsFillMoonFill className="-translate-y-[1px] translate-x-[.2px]" />
+                    </label>
+                </div>
+            </div>
+        </>
+    )
 }
-
-export default App
